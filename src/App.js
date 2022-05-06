f9d00b70e34b731a449530a54c6e0496bcb7b27e
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2022-05-05 21:24:48 -0300

    Renderização condicional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
-import Membro from './components/Membro'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Membro nome="Visitante"/>
-      </div>
-    )
-  }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			status: false
+		}
+		this.sair = this.sair.bind(this)
+		this.entrar = this.entrar.bind(this)
+	}
+
+	sair(){
+		this.setState({status: false});
+	}
+
+	entrar(){
+		this.setState({status: true});
+	}
+
+
+	render() {
+		return (
+			<div>
+				{this.state.status ? 
+					<div>
+						<h2>Bem-vindo ao sistema</h2>
+						<button onClick={this.sair}>Sair</button>
+					</div>
+				: 
+					<div>
+						<h2>Olá visitante, faça o login!</h2>
+						<button onClick={this.entrar}>Entrar no sistema</button>
+					</div>
+				}
+			</div>
+		)
+	}
 }
 
 export default App;
