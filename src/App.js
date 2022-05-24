af0981075783cda8d4e8ccc754409615df5a2553
Andre Alves Araujo <andrealvesaraujo.1995@gmail.com>
2022-05-24 19:06:03 -0300

    UseMemo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 function App(){
 
-	const [tarefas, setTarefas] = useState([
-		'Pagar a conta de luz',
-		'Estudar React Hooks',
-	])
-
+	const [tarefas, setTarefas] = useState([])
 	const [input, setInput] = useState('')
+
+	useEffect(()=>{
+		const tarefasStorage = localStorage.getItem('tarefas')
+		if(tarefasStorage){
+			setTarefas(JSON.parse(tarefasStorage))
+		}
+		
+	}, [])
+
+	useEffect(()=>{
+		localStorage.setItem('tarefas', JSON.stringify(tarefas))
+	}, [tarefas])
 
 	function handleAdd(){
 		setTarefas([...tarefas, input])
 		setInput('')
 	}
 
+	const totalTarefas = useMemo(()=>tarefas.length, [tarefas])
 
 	return (
 		<div>
@@ -22,7 +31,9 @@ function App(){
 					<li key={tarefa}>{tarefa}</li>
 				))}
 			</ul>
-
+			<br/>
+			<strong>Você tem {totalTarefas} tarefas!</strong>
+			<br/>
 			<input type="text" value={input} onChange={(e)=> setInput(e.target.value)}/>
 			<button type="button" onClick={handleAdd}>Adicionar</button>
 		</div>
